d81d6d7e9f41de30408b4f09a4495805a70e713f
Syed Shahid Ali <syedshahid1215@gmail.com>
2020-11-14 02:05:38 +0500

    remove console

--- src/components/TransactionHistory.js
+++ src/components/TransactionHistory.js
@@ -7,7 +7,7 @@ export const TransactionHistory = () => {
 
     // const context = useContext(GlobalContext); // instead of using context.transactions we can destructure
     const { transactions } = useContext(GlobalContext); // so transactions is an array we can loop through it
-    console.log(transactions);
+    // console.log(transactions);
     return (
         <div>
             <h3>Transaction History</h3>
